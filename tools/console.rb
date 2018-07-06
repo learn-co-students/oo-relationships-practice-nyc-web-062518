53c1948168ddf6c6006618fd2154812d222549d9
Rose <rose1264@gmail.com>
2018-07-06 14:57:40 -0400

    done airbnb

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,14 @@ def reload
   load 'config/environment.rb'
 end
 
+# listing_one = Listing.new("shanghai")
+# listing_two = Listing.new("new york")
+
+# guest_one = Guest.new("rose")
+# guest_two = Guest.new("joe")
+
+# trip_one = Trip.new(listing_one, guest_one)
+# trip_two = Trip.new(listing_two, guest_two)
+# trip_three = Trip.new(listing_two, guest_one)
+
 Pry.start
